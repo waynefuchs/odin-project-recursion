fe2ba84ef6abd8c95615e7c528420d162a5ed725
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-05 05:03:38 -0500

    Implement Project Euler Problem #2

---
+++ projectEuler002.js
@@ -0,0 +1,8 @@
+let fib = [];
+for(let fibLast=0, fibThis=1, fibNext=0; 
+    fibLast < 4_000_000; 
+    fibLast=fibThis,fibThis=fibNext) {
+    if(!(fibLast%2)) fib.push(fibLast);
+    fibNext = fibLast + fibThis;
+}
+console.log(`Even Fibonacci Numbers Under 4M: ${fib}`);
