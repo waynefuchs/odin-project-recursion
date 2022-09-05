285d00ff32bcd2155cdf0c91e94cd7fe5f81d14c
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-05 05:38:33 -0500

    Calculate sum

--- projectEuler001.js
+++ projectEuler001.js
@@ -1,13 +1,17 @@
 // Non-Recursive
-var allMultiplesOf3Or5 = (num) => [...Array(num+1).keys()].filter(n => !(n % 3) || !(n % 5)).splice(1);
+var allMultiplesOf3Or5 = (num) => [...Array(num).keys()].filter(n => !(n % 3) || !(n % 5)).splice(1);
 console.log('non-recursive');
-console.log(`${allMultiplesOf3Or5(1000)}`);
+const multiples = allMultiplesOf3Or5(1000);
+console.log(`${multiples}`);
+console.log(`Total: ${multiples.reduce((x,total) => total += x, 0)}`);
 
 // Recursive
 var allMultiplesOf3Or5Recursive = (num) => {
     return num > 3
-        ? allMultiplesOf3Or5Recursive(num-1).concat(!(num % 3) || !(num % 5) ? num : [])
+        ? allMultiplesOf3Or5Recursive(num-1).concat( (!(num % 3) || !(num % 5)) && num < 1000 ? num : [])
         : [3];
 };
 console.log('recursive');
-console.log(`${allMultiplesOf3Or5Recursive(1000)}`);
+const multiplesRecursive = allMultiplesOf3Or5Recursive(1000);
+console.log(`${multiplesRecursive}`);
+console.log(`Total: ${multiplesRecursive.reduce((x,total) => total += x, 0)}`);
